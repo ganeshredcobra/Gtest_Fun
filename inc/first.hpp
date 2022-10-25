87dd62235c8b3b2b9c965f22161a153efe04ea78
Ganesh <ganeshredcobra@gmail.com>
2022-10-25 16:20:45 +0200

    Create first.hpp

---
+++ inc/first.hpp
@@ -0,0 +1,16 @@
+#ifndef _FIRST_HPP_
+#define _FIRST_HPP_
+
+class InitFirst
+{
+private:
+    /* data */
+public:
+    InitFirst(/* args */);
+    ~InitFirst();
+    int FindValues(int x, int y, int z);
+    int FindAndThrow(int x, int y, int z);
+};
+
+
+#endif /* _FIRST_HPP_ */
