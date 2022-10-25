8fd6ed92ad61b6125e187d97038a6042e613484e
Ganesh <ganeshredcobra@gmail.com>
2022-10-25 16:36:07 +0200

    Update first.cpp

--- src/first.cpp
+++ src/first.cpp
@@ -1,30 +1,18 @@
 #include "first.hpp"
 #include <stdexcept>
 
-InitFirst::InitFirst(/* args */)
-{
+auto First::InitFirst::FindValues(int x, int y, int z) -> int {
+  if ((x < First::xlimit) || (y > First::ylimit) || (z != First::zlimit)) {
+    return -1;
+  }
+
+  return 0;
 }
 
-InitFirst::~InitFirst()
-{
-}
+auto First::InitFirst::FindAndThrow(int x, int y, int z) -> int {
+  if ((x < First::xlimit) || (y > First::ylimit) || (z != First::zlimit)) {
+    throw std::runtime_error("Given values are not correct");
+  }
 
-int InitFirst::FindValues(int x, int y, int z)
-{
-    if((x<20) || (y>30) || (z!=0))
-    {
-        return -1;
-    }
-
-    return 0;
-}
-
-int InitFirst::FindAndThrow(int x, int y, int z)
-{
-    if((x<20) || (y>30) || (z!=0))
-    {
-        throw std::runtime_error("Given values are not correct");
-    }
-
-    return 0;
+  return 0;
 }
