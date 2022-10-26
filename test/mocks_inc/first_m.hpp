be4dbf8aae9e825041e3294f5941b6f1de8b4fd4
Ganesh <ganeshredcobra@gmail.com>
2022-10-26 16:53:56 +0200

    Create first_m.hpp

---
+++ test/mocks_inc/first_m.hpp
@@ -0,0 +1,22 @@
+#ifndef _MOCKS_INC_FIRST_HPP_
+#define _MOCKS_INC_FIRST_HPP_
+
+#include <gmock/gmock.h>
+
+namespace First_test {
+
+constexpr int xlimit = 20;
+constexpr int ylimit = 30;
+constexpr int zlimit = 0;
+
+class MockInitFirst {
+private:
+  /* data */
+public:
+  MOCK_METHOD(int, FindValues, (int x, int y, int z), ());
+  MOCK_METHOD(int, FindAndThrow, (int x, int y, int z), ());
+};
+
+} // namespace First
+
+#endif /* _MOCKS_INC_FIRST_HPP_ */
