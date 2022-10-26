07403ce73476c6430cc89b3121396ee51f68001d
Ganesh <ganeshredcobra@gmail.com>
2022-10-26 16:47:53 +0200

    Update first_test.cpp

--- test/first_test.cpp
+++ test/first_test.cpp
@@ -1,5 +1,20 @@
 #include <gtest/gtest.h>
 
+#include "mocks_inc/first_m.hpp"
+
+using ::testing::_;
+using ::testing::Return;
+using ::testing::StrictMock;
+
 TEST(firstTest, trial) {
   EXPECT_TRUE( 1 == 1 );
+}
+
+TEST(firstTest, FindValues) {
+    First_test::MockInitFirst newObj;
+
+    EXPECT_CALL(newObj, FindValues).WillOnce(Return(0));
+
+    newObj.FindValues(10,20,30);
+    
 }
